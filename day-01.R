3fc5e539d3c278f0e5bcef26167453d86c6033b1
Paulo Villarroel <pvillarroeltapia@gmail.com>
2022-12-02 09:06:05 -0300

    Update script

--- day-01.R
+++ day-01.R
@@ -1,13 +1,21 @@
 ## --- Day 1: Calorie Counting ---
 
+## Part 1
 library(tidyverse)
 
 input <- read_delim("inputs/input-1.txt", delim = "\n", skip_empty_rows = FALSE, col_names = FALSE) |> 
   unnest(cols = c())
 
-input |> 
+elf_calories <- input |> 
   mutate(elf = cumsum(lag(is.na(X1), default = TRUE))) |> 
   group_by(elf) |> 
   summarise(sum_calories = sum(X1, na.rm = TRUE)) |> 
-  arrange(desc(sum_calories)) |> 
+  arrange(desc(sum_calories))
+
+elf_calories
+
+## Part 2
+most_calories <- elf_calories |> 
   head(3)
+
+sum(most_calories$sum_calories)
