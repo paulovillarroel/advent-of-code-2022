69afd2a6eb1658bbf415c6ed35c056a97ffcc526
Paulo Villarroel <pvillarroeltapia@gmail.com>
2022-12-05 23:44:52 -0300

    Fix styles

--- day-05.R
+++ day-05.R
@@ -12,60 +12,58 @@ moves <- input |>
   mutate_if(is.character, as.numeric)
 
 # Code heavily influenced by David Robinson and his grid_tidy() function https://github.com/dgrtwo/adventdrob/blob/main/R/grid.R"
-stacks <- input |> 
-  filter(!str_detect(X1, "move")) |> 
-  mutate(row = row_number()) |> 
-  mutate(value = stringr::str_split(X1, "")) |> 
-  select(-X1) |> 
+stacks <- input |>
+  filter(!str_detect(X1, "move")) |>
+  mutate(row = row_number()) |>
+  mutate(value = str_split(X1, "")) |>
+  select(-X1) |>
   unnest(value) |>
   mutate(col = rep(1:35, times = 9)) |>
-  filter(str_detect(value, "[A-Z]")) |> 
-  arrange(desc(row)) |> 
-  mutate(crate = match(col, unique(sort(col)))) |> 
-  select(crate, value) |> 
-  group_by(crate) |> 
-  summarise(crates = list(value)) |> 
+  filter(str_detect(value, "[A-Z]")) |>
+  arrange(desc(row)) |>
+  mutate(crate = match(col, unique(sort(col)))) |>
+  select(crate, value) |>
+  group_by(crate) |>
+  summarise(crates = list(value)) |>
   deframe()
 
-for (i in 1:nrow(moves)){
-  
+for (i in 1:nrow(moves)) {
   movement <- moves[i, ]
-  
+
   to_move <- rev(tail(stacks[[movement$from]], movement$count))
   stacks[[movement$from]] <- head(stacks[[movement$from]], -movement$count)
   stacks[[movement$to]] <- c(stacks[[movement$to]], to_move)
 }
 
-stacks |> 
-  map_chr(last) |> 
+stacks |>
+  map_chr(last) |>
   paste(collapse = "")
 
 
 ## Part 2
-stacks <- input |> 
-  filter(!str_detect(X1, "move")) |> 
-  mutate(row = row_number()) |> 
-  mutate(value = stringr::str_split(X1, "")) |> 
-  select(-X1) |> 
+stacks <- input |>
+  filter(!str_detect(X1, "move")) |>
+  mutate(row = row_number()) |>
+  mutate(value = str_split(X1, "")) |>
+  select(-X1) |>
   unnest(value) |>
   mutate(col = rep(1:35, times = 9)) |>
-  filter(str_detect(value, "[A-Z]")) |> 
-  arrange(desc(row)) |> 
-  mutate(crate = match(col, unique(sort(col)))) |> 
-  select(crate, value) |> 
-  group_by(crate) |> 
-  summarise(crates = list(value)) |> 
+  filter(str_detect(value, "[A-Z]")) |>
+  arrange(desc(row)) |>
+  mutate(crate = match(col, unique(sort(col)))) |>
+  select(crate, value) |>
+  group_by(crate) |>
+  summarise(crates = list(value)) |>
   deframe()
 
-for (i in 1:nrow(moves)){
-  
+for (i in 1:nrow(moves)) {
   movement <- moves[i, ]
-  
+
   to_move <- tail(stacks[[movement$from]], movement$count)
   stacks[[movement$from]] <- head(stacks[[movement$from]], -movement$count)
   stacks[[movement$to]] <- c(stacks[[movement$to]], to_move)
 }
 
-stacks |> 
-  map_chr(last) |> 
+stacks |>
+  map_chr(last) |>
   paste(collapse = "")
