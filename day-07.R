79b4c7469f5e792b4dc9d8d648d2f57907db17f7
Paulo Villarroel <pvillarroeltapia@gmail.com>
2022-12-08 17:52:13 -0300

    Upload day 7

---
+++ day-07.R
@@ -0,0 +1,51 @@
+## --- Day 7: No Space Left On Device ---
+
+## Part 1 
+
+library(tidyverse)
+
+input <- read_csv("inputs/input-07.txt", col_names = c("line"), show_col_types = FALSE)
+
+cd <- function(path, dir = NA) {
+  if (any(is.na(dir))) {
+    return(path)
+  }
+  if (any(dir == "..")) {
+    return(head(path, -1))
+  }
+  return(c(path, paste0(tail(path, 1), "/", dir)))
+}
+
+paths <- input |>
+  mutate(path = line |>
+    str_extract("cd (.*)") |>
+    str_remove("cd ")) |>
+  mutate(path = accumulate(path, cd)) |>
+  unnest(path)
+
+sizes <- paths |>
+  filter(str_detect(line, "^[0-9]")) |>
+  group_by(path) |>
+  summarize(size = line |>
+    str_extract("^[0-9]+") |>
+    as.numeric() |>
+    sum()) |>
+  arrange(-size)
+
+sizes |>
+  filter(size < 100000) |>
+  pull(size) |>
+  sum()
+
+
+## Part 2
+total <- 70000000
+required <- 30000000
+
+used <- head(sizes, 1) |>
+  pull(size)
+
+sizes |>
+  filter(size >= (required - (total - used))) |>
+  arrange(size) |>
+  head(1)
